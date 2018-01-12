e3b80ab38a948c26af311d216a33612a0bb7dfac
gfleetwood <gfleetwood@protonmail.com>
2018-01-11 22:21:56 -0500

    PyPi!
    The half-baked version is now pip installable.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+  name='handyman',
+  version='0.1',
+  description='Miscellaneous Data Science functions',
+  url='https://github.com/gfleetwood/handyman',
+  author='Gordon Fleetwood',
+  author_email='gfleetwood@protonmail.com',
+  license='MIT',
+  packages=['handyman'],
+  zip_safe=False
+      )
