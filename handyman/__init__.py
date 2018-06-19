515ea1a418be2b5918b089cde12d3e1488a2e551
Gordon Fleetwood <gfleetwood@protonmail.com>
2018-06-19 14:35:50 -0400

    Update __init__.py

--- handyman/__init__.py
+++ handyman/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ['helper_functions']
-from .helper_functions import impute_scale_data, get_dummies_enhanced, cutoff_youdens_j, \
+from .helper_functions import impute_scale_data, cutoff_youdens_j, \
 data_diagnostics, get_num_corr_plot, classification_metrics, kaiser_harris_criterion,\
-serialize_model, unserialize_model, rf_feature_importance, flatten_dict, flatten_list,\
+serialize_model, unserialize_model, rf_feature_importance, flatten_dict, \
 get_coefficients, get_date_time 
