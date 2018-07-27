3b170002b255fa46bbb0d39036c23a5952a58e9c
gfleetwood <gfleetwood@protonmail.com>
2018-07-27 06:22:19 -0400

    Update

--- handyman/helper_functions.py
+++ handyman/helper_functions.py
@@ -9,6 +9,17 @@ from collections import OrderedDict
 from sklearn.metrics import roc_curve as rc
 from sklearn.preprocessing import Imputer
 from sklearn.preprocessing import StandardScaler
+import statsmodels.formula.api as sm
+import seaborn as sns
+import matplotlib.pyplot as plt
+import statsmodels.formula.api as smf
+from statsmodels.graphics.gofplots import ProbPlot
+
+plt.style.use('seaborn')
+plt.rc('font', size=14)
+plt.rc('figure', titlesize=18)
+plt.rc('axes', labelsize=15)
+plt.rc('axes', titlesize=18)
 
 def impute_scale_data(df):
     '''
@@ -337,3 +348,136 @@ def get_date_time():
     * * A list contains two elements as strings: 1) date, and 2) time.
     '''
     return str(datetime.datetime.now()).split(' ')
+    
+def ols_with_diagnostics(df, formula = 'sepal_length ~ sepal_width'):
+    # https://medium.com/@emredjan/emulating-r-regression-plots-in-python-43741952c034
+    
+    model_fit = sm.ols(formula, data = df).fit()
+
+    # fitted values (need a constant term for intercept)
+    model_fitted_y = model_fit.fittedvalues
+
+    # model residuals
+    model_residuals = model_fit.resid
+
+    # normalized residuals
+    model_norm_residuals = model_fit.get_influence().resid_studentized_internal
+
+    # absolute squared normalized residuals
+    model_norm_residuals_abs_sqrt = np.sqrt(np.abs(model_norm_residuals))
+
+    # absolute residuals
+    model_abs_resid = np.abs(model_residuals)
+
+    # leverage, from statsmodels internals
+    model_leverage = model_fit.get_influence().hat_matrix_diag
+
+    # cook's distance, from statsmodels internals
+    model_cooks = model_fit.get_influence().cooks_distance[0]
+    
+    plot_lm_1 = plt.figure(1)
+    plot_lm_1.set_figheight(8)
+    plot_lm_1.set_figwidth(12)
+
+    plot_lm_1.axes[0] = sns.residplot(model_fitted_y, 'sepal_length', data=df, 
+                              lowess=True, 
+                              scatter_kws={'alpha': 0.5}, 
+                              line_kws={'color': 'red', 'lw': 1, 'alpha': 0.8})
+
+    plot_lm_1.axes[0].set_title('Residuals vs Fitted')
+    plot_lm_1.axes[0].set_xlabel('Fitted values')
+    plot_lm_1.axes[0].set_ylabel('Residuals')
+
+    # annotations
+    abs_resid = model_abs_resid.sort_values(ascending=False)
+    abs_resid_top_3 = abs_resid[:3]
+
+    for i in abs_resid_top_3.index:
+        plot_lm_1.axes[0].annotate(i, 
+                                   xy=(model_fitted_y[i], 
+                                       model_residuals[i]));
+        
+    QQ = ProbPlot(model_norm_residuals)
+    plot_lm_2 = QQ.qqplot(line='45', alpha=0.5, color='#4C72B0', lw=1)
+
+    plot_lm_2.set_figheight(8)
+    plot_lm_2.set_figwidth(12)
+
+    plot_lm_2.axes[0].set_title('Normal Q-Q')
+    plot_lm_2.axes[0].set_xlabel('Theoretical Quantiles')
+    plot_lm_2.axes[0].set_ylabel('Standardized Residuals');
+
+    # annotations
+    abs_norm_resid = np.flip(np.argsort(np.abs(model_norm_residuals)), 0)
+    abs_norm_resid_top_3 = abs_norm_resid[:3]
+
+    for r, i in enumerate(abs_norm_resid_top_3):
+        plot_lm_2.axes[0].annotate(i, 
+                                   xy=(np.flip(QQ.theoretical_quantiles, 0)[r],
+                                       model_norm_residuals[i]));
+        
+    plot_lm_3 = plt.figure(3)
+    plot_lm_3.set_figheight(8)
+    plot_lm_3.set_figwidth(12)
+
+    plt.scatter(model_fitted_y, model_norm_residuals_abs_sqrt, alpha=0.5)
+    sns.regplot(model_fitted_y, model_norm_residuals_abs_sqrt, 
+                scatter=False, 
+                ci=False, 
+                lowess=True,
+                line_kws={'color': 'red', 'lw': 1, 'alpha': 0.8})
+
+    plot_lm_3.axes[0].set_title('Scale-Location')
+    plot_lm_3.axes[0].set_xlabel('Fitted values')
+    plot_lm_3.axes[0].set_ylabel('$\sqrt{|Standardized Residuals|}$');
+
+    # annotations
+    abs_sq_norm_resid = np.flip(np.argsort(model_norm_residuals_abs_sqrt), 0)
+    abs_sq_norm_resid_top_3 = abs_sq_norm_resid[:3]
+
+    for i in abs_norm_resid_top_3:
+        plot_lm_3.axes[0].annotate(i, 
+                                   xy=(model_fitted_y[i], 
+                                       model_norm_residuals_abs_sqrt[i]));
+        
+    plot_lm_4 = plt.figure(4)
+    plot_lm_4.set_figheight(8)
+    plot_lm_4.set_figwidth(12)
+
+    plt.scatter(model_leverage, model_norm_residuals, alpha=0.5)
+    sns.regplot(model_leverage, model_norm_residuals, 
+                scatter=False, 
+                ci=False, 
+                lowess=True,
+                line_kws={'color': 'red', 'lw': 1, 'alpha': 0.8})
+
+    plot_lm_4.axes[0].set_xlim(0, 0.20)
+    plot_lm_4.axes[0].set_ylim(-3, 5)
+    plot_lm_4.axes[0].set_title('Residuals vs Leverage')
+    plot_lm_4.axes[0].set_xlabel('Leverage')
+    plot_lm_4.axes[0].set_ylabel('Standardized Residuals')
+
+    # annotations
+    leverage_top_3 = np.flip(np.argsort(model_cooks), 0)[:3]
+
+    for i in leverage_top_3:
+        plot_lm_4.axes[0].annotate(i, 
+                                   xy=(model_leverage[i], 
+                                       model_norm_residuals[i]))
+
+    # shenanigans for cook's distance contours
+    def graph(formula, x_range, label=None):
+        x = x_range
+        y = formula(x)
+        plt.plot(x, y, label=label, lw=1, ls='--', color='red')
+
+    p = len(model_fit.params) # number of model parameters
+
+    graph(lambda x: np.sqrt((0.5 * p * (1 - x)) / x), 
+          np.linspace(0.001, 0.200, 50), 
+          'Cook\'s distance') # 0.5 line
+    graph(lambda x: np.sqrt((1 * p * (1 - x)) / x), 
+          np.linspace(0.001, 0.200, 50)) # 1 line
+    plt.legend(loc='upper right');
+    
+    return('Done')
