c2a0037238f05236ec8146cc61f50a4607f4cf49
Gordon Fleetwood <gfleetwood@protonmail.com>
2022-05-18 23:05:00 -0400

    Update

--- handyman/read_write.py
+++ handyman/read_write.py
@@ -37,3 +37,31 @@ def read_files_in_dir():
   ]
 
   return(files)
+  
+from subprocess import Popen, PIPE
+import pandas as pd
+from sqlalchemy import create_engine
+import pyodbc
+import urllib
+import numpy as np
+import psycopg2
+
+def read_db_data_thru_tunnel(qry):
+
+  cmd = [
+    "ssh", 
+    "-i",
+    "C:", 
+    "-N",
+    "-L", 
+    "localhost",
+    "ec2"    
+  ]
+
+  p = Popen(cmd, stdout = PIPE, shell = False)
+  con_str = "postgresql+psycopg2://UN:PW@localhost:5432/DB"
+  eng = create_engine(con_str)
+  df = pd.read_sql(qry, eng)
+  p.kill()
+  
+  return(df)
