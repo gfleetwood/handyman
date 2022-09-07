964c3772cc8ddc0d3c421995b5c5faed0b68134b
gfleetwood <gfleetwood@protonmail.com>
2022-09-07 03:31:24 -0400

    Update general.py

--- handyman/general.py
+++ handyman/general.py
@@ -4,6 +4,28 @@ from pandas import DataFrame
 from sqlalchemy import create_engine
 import os
 
+def impute_scale_data(df):
+
+    '''
+    Imputes missing values with the median for numeric features and the mode for categorical ones, 
+    and then scales the numeric features by subtracting the mean and dividing by the standard deviation.
+    '''    
+
+    imputer_num = sk_pp.Imputer(strategy = 'median') 
+    df.loc[:, df.select_dtypes(exclude = ['object']).columns] = \
+    imputer_num.fit_transform(df.select_dtypes(exclude = ['object']))  
+    
+    scaler = sk_pp.StandardScaler()
+    df.loc[:, df.select_dtypes(exclude = ['object']).columns] = \
+    scaler.fit_transform(df.select_dtypes(exclude = ['object']))
+    
+    for col in df.select_dtypes(include = ['object']).columns:
+        df.loc[:, col] = df.loc[:, col].fillna(value = df.loc[:, col].value_counts().index[0])
+        
+    df_imputed_scaled = df
+        
+    return(df_imputed_scaled)
+
 def euclidean_distance(p1, p2):
 
   temp = np.array(p1) - np.array(p2) 
